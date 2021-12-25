ae827c6d16ded28156c8a97fa395609d4219573b
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2021-12-25 21:01:18 +0100

    rajout de librairies

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include "string.h"
+#include "errno.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "string.h"
 
 int main() {
     printf("Hello, World!\n");
